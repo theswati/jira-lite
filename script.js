16a0451bfcdd3aeebf1a2e891ab061cca86cd2b4
Swati <jswati603@gmail.com>
2024-03-27 12:18:01 +0530

    jira-board

--- script.js
+++ script.js
@@ -24,6 +24,33 @@ function addTask(){
             blurredCard.remove();
         }
 })
+// problem 2:make default text empty 
+card.addEventListener("click",(eventDetails)=>{
+    let clickedCard=eventDetails.target;
+    console.log(clickedCard)
+    if(clickedCard.innerText=="Test Card"){
+        clickedCard.innerText = ""; // 
+    }
+})
+
+let selector=document.createElement("select")
+selector.innerHTML=`
+<option value="todo">todo</option>
+<option value="progress">Progress</option>
+<option value="done">Done</option>
+`
+
+card.append(selector)
+
+selector.addEventListener("change",(eventDetails)=>{
+    let selectedoption=eventDetails.target.value //todo1 progress1 done1
+     let selectedContainer=document.querySelector(`#${selectedoption}`)
+     selectedContainer.append(card);
+
+
+
+})
+
 
 
 
